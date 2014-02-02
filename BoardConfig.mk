79a229081bb40d7d2ffa4efb7b04fdfd10860e14
James Taylor <jmztaylor@gmail.com>
2014-02-02 20:06:55 +0000

    Add TARGET_POWERHAL_NO_TOUCH_BOOST flag
    Change-Id: Ie59116a941afc2c84396e748150140bd6139b289

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,9 @@ BOARD_VOLD_MAX_PARTITIONS := 36
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
+# Power
+TARGET_POWERHAL_NO_TOUCH_BOOST := true
+
 # Recovery
 TARGET_RECOVERY_FSTAB := device/htc/m4/rootdir/etc/fstab.m4
 BOARD_HAS_NO_SELECT_BUTTON := true
